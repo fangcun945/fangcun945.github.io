d9c4b5e590b773b61195874637d0d2f38bed810b
fangcun945 <473452759@qq.com>
2025-04-28 00:35:05 +0800

    Site updated: 2025-04-28 00:35:05

---
+++ js/cursor-effect.js
@@ -0,0 +1,14 @@
+/* 常规光标 */
+body {
+  cursor: url('/images/cursor-normal.cur'), auto;
+}
+
+/* 链接悬停效果 */
+a:hover, button:hover {
+  cursor: url('/images/cursor-hover.cur'), pointer;
+}
+
+/* 代码区块特殊光标 */
+pre:hover {
+  cursor: url('/images/cursor-code.cur'), text;
+}
